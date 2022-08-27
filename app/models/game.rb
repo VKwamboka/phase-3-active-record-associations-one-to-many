37a955402eff733c6b2a64d1341c6203786ba6ac
verah <verahnyagaka101@gmail.com>
2022-08-28 01:22:50 +0300

    completed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ActiveRecord::Base
-  
+    # a review belongs to a game
+    # def game
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+    # end
+
+    has_many :reviews
+    
 end
